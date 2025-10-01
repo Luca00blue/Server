2171a33af073b41d8377a5dc125db3a99711cb9a
lai2-24 <lai2-24@itismeucci.com>
2025-10-01 10:02:46 +0200

    esbufferprint

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,12 @@
 package com.example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.Buffer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,6 +14,8 @@ public class Main {
         ServerSocket mioserver= new ServerSocket(3000);
         Socket mioSocket = mioserver.accept();
         System.out.println("qualcuno si e collegato");
-        
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(mioSocket.getOutputStream(), true);
     }
 }
